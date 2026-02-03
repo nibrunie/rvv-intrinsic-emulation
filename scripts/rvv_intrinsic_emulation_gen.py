600732a408a86e3c91ded17841613adb68ce5cfa
Nicolas Brunie <nibrunie@gmail.com>
2026-02-02 22:00:12 -0800

    Generating all variant of u8/u16/u32/u64 vector left and right rotations

--- scripts/rvv_intrinsic_emulation_gen.py
+++ scripts/rvv_intrinsic_emulation_gen.py
@@ -44,19 +44,27 @@ class LMULType(Enum):
 
 class OperationType(Enum):
     ROR = auto()
-    ROTL = auto()
-    ROXR = auto()
-    ROXL = auto()
+    ROL = auto()
     SLL = auto()
     SRL = auto()
     SRA = auto()
     NSRL = auto() 
-    RSUB = auto()
     ADD = auto()
     SUB = auto()
+    RSUB = auto()
     OR = auto()
     AND = auto()
     XOR = auto()
+    MUL = auto()
+    DIV = auto()
+    REM = auto()
+    NOT = auto()
+    EQ = auto()
+    NE = auto()
+    LT = auto()
+    LE = auto()
+    GT = auto()
+    GE = auto()
 
     INPUT = auto()
     IMMEDIATE = auto()
@@ -65,12 +73,8 @@ class OperationType(Enum):
     def to_string(op_type: 'OperationType') -> str:
         if op_type == OperationType.ROR:
             return "ror"
-        elif op_type == OperationType.ROTL:
-            return "rotl"
-        elif op_type == OperationType.ROXR:
-            return "roxr"
-        elif op_type == OperationType.ROXL:
-            return "roxl"
+        elif op_type == OperationType.ROL:
+            return "rol"
         elif op_type == OperationType.SLL:
             return "sll"
         elif op_type == OperationType.SRL:
@@ -95,33 +99,34 @@ class OperationType(Enum):
             raise ValueError("Invalid operation type")
 
 class OperationDesciptor:
-    def __init__(self, op_type, dst_type):
+    def __init__(self, op_type):
         self.op_type = op_type
-        self.dst_type = dst_type
 
 class Node:
     def __init__(self):
         self.node_type = NodeType.UNDEFINED
+        self.node_format = None
 
 class Immediate(Node):
-    def __init__(self, node_descriptor: NodeDescriptor, value: int):
-        self.node_descriptor = node_descriptor
+    def __init__(self, node_format: NodeFormatDescriptor, value: int):
+        self.node_format = node_format
         self.value = value
         self.node_type = NodeType.IMMEDIATE
 
 class Input(Node):
-    def __init__(self, node_descriptor: NodeDescriptor, index: int):
-        self.node_descriptor = node_descriptor
+    def __init__(self, node_format: NodeFormatDescriptor, index: int):
+        self.node_format = node_format
         self.index = index
         self.node_type = NodeType.INPUT
 
 class Operation(Node):
-    def __init__(self, op_desc: OperationDesciptor, *args):
+    def __init__(self, node_format: NodeFormatDescriptor, op_desc: OperationDesciptor, *args):
+        self.node_format = node_format
         self.op_desc = op_desc
         self.args = args
         self.node_type = NodeType.OPERATION
 
-class OperandType(Enum):
+class NodeFormatType(Enum):
     VECTOR = auto()
     SCALAR = auto()
     IMMEDIATE = auto()
@@ -132,71 +137,70 @@ class NodeType(Enum):
     OPERATION = auto()
     UNDEFINED = auto()
 
-class NodeDescriptor:
-    def __init__(self, operand_type, int_type, lmul_type, node_type = NodeType.UNDEFINED):
-        self.operand_type = operand_type
-        self.int_type = int_type
+class NodeFormatDescriptor:
+    def __init__(self, node_format_type: NodeFormatType, elt_type: IntType, lmul_type: LMULType=None):
+        self.node_format_type = node_format_type
+        self.elt_type = elt_type
         self.lmul_type = lmul_type
-        self.node_type = node_type
 
-class OperationDescriptor(NodeDescriptor):
-    def __init__(self, op_type, dst_type):
-        super().__init__(OperandType.VECTOR, dst_type, LMULType.M1, NodeType.OPERATION)
-        self.op_type = op_type
-
-class InputDescriptor(NodeDescriptor):
-    def __init__(self, node_descriptor: NodeDescriptor):
-        super().__init__(node_descriptor.operand_type, node_descriptor.int_type, node_descriptor.lmul_type)
-        self.node_type = NodeType.INPUT
-
-class OperandDescriptor(NodeDescriptor):
-    def __init__(self, node_descriptor: NodeDescriptor, index: int):
-        super().__init__(node_descriptor.operand_type, node_descriptor.int_type, node_descriptor.lmul_type)
-        self.index = index
-        self.node_type = NodeType.INPUT
-
-    @property
-    def node_descriptor(self) -> NodeDescriptor:
-        return NodeDescriptor(self.operand_type, self.int_type, self.lmul_type)
-
-class ImmediateDescriptor(NodeDescriptor):
-    def __init__(self, node_descriptor: NodeDescriptor, value: int):
-        super().__init__(node_descriptor.operand_type, node_descriptor.int_type, node_descriptor.lmul_type)
-        self.value = value
-        self.node_type = NodeType.IMMEDIATE
-
-
-def element_size(int_type: IntType) -> int:
-    if int_type == IntType.U8 or int_type == IntType.S8:
+def element_size(elt_type: IntType) -> int:
+    if elt_type == IntType.U8 or elt_type == IntType.S8:
         return 8
-    elif int_type == IntType.U16 or int_type == IntType.S16:
+    elif elt_type == IntType.U16 or elt_type == IntType.S16:
         return 16
-    elif int_type == IntType.U32 or int_type == IntType.S32:
+    elif elt_type == IntType.U32 or elt_type == IntType.S32:
         return 32
-    elif int_type == IntType.U64 or int_type == IntType.S64:
+    elif elt_type == IntType.U64 or elt_type == IntType.S64:
         return 64
     else:
         raise ValueError("Invalid integer type")
 
+def get_scalar_format(node_format: NodeFormatDescriptor) -> NodeFormatDescriptor:
+    return NodeFormatDescriptor(NodeFormatType.SCALAR, node_format.elt_type, None)
 
 # description of vector rotation emulation 
-def rotate_left(elts: OperandDescriptor, rot_amount: OperandDescriptor) -> vtype_t:
+def rotate_left(elts: Node, rot_amount: Node) -> Node:
     left_shift = Operation(
-        OperationDesciptor(OperationType.SLL, elts.node_descriptor),
+        elts.node_format,
+        OperationDesciptor(OperationType.SLL),
         elts, rot_amount
     )
     right_shift = Operation(
-        OperationDesciptor(OperationType.SRL, elts.node_descriptor),
+        elts.node_format,
+        OperationDesciptor(OperationType.SRL),
         elts,
         Operation(
-            OperationDesciptor(OperationType.RSUB, elts.node_descriptor),
+            rot_amount.node_format,
+            OperationDesciptor(OperationType.RSUB),
             rot_amount,
-            ImmediateDescriptor(NodeDescriptor(OperandType.IMMEDIATE, elts.int_type, elts.lmul_type), element_size(elts.int_type))
-        )
+            Immediate(get_scalar_format(rot_amount.node_format), element_size(elts.node_format.elt_type))
+        )   
     )
      
-    or_desc = OperationDesciptor(OperationType.OR, elts.node_descriptor)
-    return Operation(or_desc, left_shift, right_shift)
+    or_desc = OperationDesciptor(OperationType.OR)
+    return Operation(elts.node_format, or_desc, left_shift, right_shift)
+
+# description of vector rotation emulation 
+def rotate_right(elts: Node, rot_amount: Node) -> Node:
+    right_shift = Operation(
+        elts.node_format,
+        OperationDesciptor(OperationType.SRL),
+        elts, rot_amount
+    )
+    left_shift = Operation(
+        elts.node_format,
+        OperationDesciptor(OperationType.SLL),
+        elts,
+        Operation(
+            rot_amount.node_format,
+            OperationDesciptor(OperationType.RSUB),
+            rot_amount,
+            Immediate(get_scalar_format(rot_amount.node_format), element_size(elts.node_format.elt_type))
+        )   
+    )
+     
+    or_desc = OperationDesciptor(OperationType.OR)
+    return Operation(elts.node_format, or_desc, left_shift, right_shift)
 
 def int_type_to_scalar_type(int_type: IntType) -> str:
     if int_type == IntType.U8:
@@ -234,17 +238,17 @@ def int_type_to_vector_type(int_type: IntType, lmul_type: LMULType) -> str:
     else:
         raise ValueError("Invalid integer type")
 
-def generate_operand_type(op_descriptor: OperandDescriptor) -> str:
-    if op_descriptor.operand_type == OperandType.VECTOR:
-        return int_type_to_vector_type(op_descriptor.int_type, op_descriptor.lmul_type)
-    elif op_descriptor.operand_type == OperandType.SCALAR:
-        return int_type_to_scalar_type(op_descriptor.int_type)
-    elif op_descriptor.operand_type == OperandType.IMMEDIATE:
-        return int_type_to_scalar_type(op_descriptor.int_type)
+def generate_node_format_type_string(node_format: NodeFormatDescriptor) -> str:
+    if node_format.node_format_type == NodeFormatType.VECTOR:
+        return int_type_to_vector_type(node_format.elt_type, node_format.lmul_type)
+    elif node_format.node_format_type == NodeFormatType.SCALAR:
+        return int_type_to_scalar_type(node_format.elt_type)
+    elif node_format.node_format_type == NodeFormatType.IMMEDIATE:
+        return int_type_to_scalar_type(node_format.elt_type)
     else:
         raise ValueError("Invalid operand type")
 
-def generate_intrinsic_type_tag(op_descriptor: OperandDescriptor) -> str:
+def generate_intrinsic_type_tag(node_format: NodeFormatDescriptor) -> str:
     type_tag = {
         IntType.U8: "u8",
         IntType.S8: "i8",
@@ -255,62 +259,175 @@ def generate_intrinsic_type_tag(op_descriptor: OperandDescriptor) -> str:
         IntType.U64: "u64",
         IntType.S64: "i64",
     }
-    return f"{type_tag[op_descriptor.int_type]}{LMULType.to_string(op_descriptor.lmul_type)}"
+    return f"{type_tag[node_format.elt_type]}{LMULType.to_string(node_format.lmul_type)}"
 
 
 def generate_intrinsic_name(prototype: Operation) -> str:
-    intrinsic_type_tag = generate_intrinsic_type_tag(prototype.op_desc.dst_type)
-    intrinsic_name = f"__riscv_v{OperationType.to_string(prototype.op_desc.op_type)}_vv_{intrinsic_type_tag}"
+    intrinsic_type_tag = generate_intrinsic_type_tag(prototype.node_format)
+    # building operand type descriptor (vv, vx, vi)
+    operand_type_descriptor = ""
+    for arg in prototype.args:
+        if arg.node_format.node_format_type == NodeFormatType.VECTOR:
+            operand_type_descriptor += "v"
+        elif arg.node_format.node_format_type == NodeFormatType.SCALAR:
+            operand_type_descriptor += "x"
+        elif arg.node_format.node_format_type == NodeFormatType.IMMEDIATE:
+            operand_type_descriptor += "i"
+    intrinsic_name = f"__riscv_v{OperationType.to_string(prototype.op_desc.op_type)}_{operand_type_descriptor}_{intrinsic_type_tag}"
     return intrinsic_name
 
 def generate_intrinsic_prototype(prototype: Operation) -> str:
     # generate intrinsic name
     intrinsic_name = generate_intrinsic_name(prototype)
     # generate prototype
-    dst_type = generate_operand_type(prototype.op_desc.dst_type)
-    src_types = [generate_operand_type(arg) for arg in prototype.args]
+    dst_type = generate_node_format_type_string(prototype.node_format)
+    src_types = [generate_node_format_type_string(arg.node_format) for arg in prototype.args]
     prototype = f"{dst_type} {intrinsic_name}({', '.join(src_types)})"
     return f"{prototype};"
 
-def generate_operation(op: Operation, memoization_map: dict[str]) -> str:
-    if op.op_desc.node_type == NodeType.INPUT:
+class CodeObject:
+    def __init__(self, code: str):
+        self.code = code
+        self.free_var_idx = 0
+
+    def append(self, code: str):
+        self.code += code
+
+    def allocate_new_free_var(self) -> str:
+        var = f"tmp{self.free_var_idx}"
+        self.free_var_idx += 1
+        return var
+
+
+def generate_operation(code: CodeObject, op: Node, memoization_map: dict[str]) -> str:
+    if op.node_type == NodeType.INPUT:
         return memoization_map[op]
-    elif op.op_desc.node_type == NodeType.IMMEDIATE:
-        return op.args[0].value
+    elif op.node_type == NodeType.IMMEDIATE:
+        return f"{op.value}"
     else:
-        # generate all sources
-        pre_body = "".join([generate_operation(arg, memoization_map) for arg in op.args])
-        call_op = f"{generate_intrinsic_name(op)}({', '.join([f'op{i}' for i, arg in enumerate(op.args)])})"
-        memoization_map[op] = call_op
-        return pre_body + call_op
+        assert op.node_type == NodeType.OPERATION
+        # generate temp variable
+        if op.node_format.node_format_type == NodeFormatType.VECTOR or any(arg.node_format.node_format_type == NodeFormatType.VECTOR for arg in op.args):
+            call_op = f"{generate_intrinsic_name(op)}({', '.join([generate_operation(code, arg, memoization_map) for arg in op.args])})"
+            temp_var = code.allocate_new_free_var()
+            memoization_map[op] = temp_var
+            code.append(f"  {generate_node_format_type_string(op.node_format)} {temp_var} = {call_op};\n")
+            return temp_var
+        else:
+            # scalar operation
+            return generate_scalar_operation(code, op, memoization_map)
+
+def generate_scalar_operation(code: CodeObject, op: Node, memoization_map: dict[str]) -> str:
+    arg_list = [generate_operation(code, arg, memoization_map) for arg in op.args]
+    if op.op_desc.op_type == OperationType.ADD:
+        expression = f"{arg_list[0]} + {arg_list[1]}"
+    elif op.op_desc.op_type == OperationType.SUB:
+        expression = f"{arg_list[0]} - {arg_list[1]}"
+    elif op.op_desc.op_type == OperationType.RSUB:
+        expression = f"{arg_list[1]} - {arg_list[0]}"
+    elif op.op_desc.op_type == OperationType.MUL:
+        expression = f"{arg_list[0]} * {arg_list[1]}"
+    elif op.op_desc.op_type == OperationType.DIV:
+        expression = f"{arg_list[0]} / {arg_list[1]}"
+    elif op.op_desc.op_type == OperationType.REM:
+        expression = f"{arg_list[0]} % {arg_list[1]}"
+    elif op.op_desc.op_type == OperationType.AND:
+        expression = f"{arg_list[0]} & {arg_list[1]}"
+    elif op.op_desc.op_type == OperationType.OR:
+        expression = f"{arg_list[0]} | {arg_list[1]}"
+    elif op.op_desc.op_type == OperationType.XOR:
+        expression = f"{arg_list[0]} ^ {arg_list[1]}"
+    elif op.op_desc.op_type == OperationType.NOT:
+        expression = f"~{arg_list[0]}"
+    elif op.op_desc.op_type == OperationType.SLL:
+        expression = f"{arg_list[0]} << {arg_list[1]}"
+    elif op.op_desc.op_type == OperationType.SRL:
+        expression = f"{arg_list[0]} >> {arg_list[1]}"
+    elif op.op_desc.op_type == OperationType.SRA:
+        expression = f"{arg_list[0]} >> {arg_list[1]}"
+    elif op.op_desc.op_type == OperationType.ROL:
+        expression = f"{arg_list[0]} << {arg_list[1]} | {arg_list[0]} >> (64 - {arg_list[1]})"
+    elif op.op_desc.op_type == OperationType.ROR:
+        expression = f"{arg_list[0]} >> {arg_list[1]} | {arg_list[0]} << (64 - {arg_list[1]})"
+    elif op.op_desc.op_type == OperationType.EQ:
+        expression = f"{arg_list[0]} == {arg_list[1]}"
+    elif op.op_desc.op_type == OperationType.NE:
+        expression = f"{arg_list[0]} != {arg_list[1]}"
+    elif op.op_desc.op_type == OperationType.LT:
+        expression = f"{arg_list[0]} < {arg_list[1]}"
+    elif op.op_desc.op_type == OperationType.LE:
+        expression = f"{arg_list[0]} <= {arg_list[1]}"
+    elif op.op_desc.op_type == OperationType.GT:
+        expression = f"{arg_list[0]} > {arg_list[1]}"
+    elif op.op_desc.op_type == OperationType.GE:
+        expression = f"{arg_list[0]} >= {arg_list[1]}"
+    else:
+        raise ValueError(f"Invalid operation type: {op.op_desc.op_type}")
+    
+    temp_var = code.allocate_new_free_var()
+    memoization_map[op] = temp_var
+    code.append(f"  {generate_node_format_type_string(op.node_format)} {temp_var} = {expression};\n")
+    return temp_var
+    
 
 def generate_intrinsic_from_operation(prototype: Operation, emulation: Operation) -> str:
     intrinsic_name = generate_intrinsic_name(prototype)
     # generate body
-    dst_type = generate_operand_type(prototype.op_desc.dst_type)
-    src_types = [generate_operand_type(arg) for arg in prototype.args]
+    dst_type = generate_node_format_type_string(prototype.node_format)
+    src_types = [generate_node_format_type_string(arg.node_format) for arg in prototype.args]
     src_list = [f"{src_type} op{i}" for i, src_type in enumerate(src_types)]
     memoisation_map = {src: f"op{i}" for i, src in enumerate(prototype.args)}
     header = f"{dst_type} {intrinsic_name}({', '.join(src_list)}) {{\n"
-    body = generate_operation(emulation, memoisation_map)
-    footer = "}"
-    return header + body + footer
+    code = CodeObject("")
+    result = generate_operation(code, emulation, memoisation_map)
+    footer = f"  return {result};\n}}"
+    return header + code.code + footer
 
     
-    
-    
+for elt_type in [IntType.U8, IntType.U16, IntType.U32, IntType.U64]:
+    uint_t = NodeFormatDescriptor(NodeFormatType.SCALAR, elt_type, lmul_type=None)
+    for lmul in [LMULType.M1, LMULType.M2, LMULType.M4, LMULType.M8]:
+        vuintm_t = NodeFormatDescriptor(NodeFormatType.VECTOR, elt_type, lmul)
+        
+        lhs = Input(vuintm_t, 0)
+        rhs = Input(vuintm_t, 1)
+        rhs_vx = Input(uint_t, 1)
 
-lhs = OperandDescriptor(NodeDescriptor(OperandType.VECTOR, IntType.U64, LMULType.M1), 0)
-rhs = OperandDescriptor(NodeDescriptor(OperandType.VECTOR, IntType.U64, LMULType.M1), 1)
+        vuintm_vror_vv_prototype = Operation(
+            vuintm_t,
+            OperationDesciptor(OperationType.ROR),
+            lhs,
+            rhs
+        )
+        vuintm_vror_vv_emulation = rotate_right(lhs, rhs)
 
-vuint64m1_vror_vv_prototype = Operation(
-    OperationDesciptor(OperationType.ROR, NodeDescriptor(OperandType.VECTOR, IntType.U64, LMULType.M1)),
-    lhs,
-    rhs
-)
-vuint64m1_vror_vv_emulation = rotate_left(lhs, rhs)
+        vuintm_vror_vx_prototype = Operation(
+            vuintm_t,
+            OperationDesciptor(OperationType.ROR),
+            lhs,
+            rhs_vx
+        )
+        vuintm_vror_vx_emulation = rotate_right(lhs, rhs_vx)
 
-print("prototype")
-print(generate_intrinsic_prototype(vuint64m1_vror_vv_prototype))
-print("intrinsic")
-print(generate_intrinsic_from_operation(vuint64m1_vror_vv_prototype, vuint64m1_vror_vv_emulation))
+        vuintm_vrol_vv_prototype = Operation(
+            vuintm_t,
+            OperationDesciptor(OperationType.ROL),
+            lhs,
+            rhs
+        )
+        vuintm_vrol_vv_emulation = rotate_left(lhs, rhs)
+
+        vuintm_vrol_vx_prototype = Operation(
+            vuintm_t,
+            OperationDesciptor(OperationType.ROL),
+            lhs,
+            rhs_vx
+        )
+        vuintm_vrol_vx_emulation = rotate_left(lhs, rhs_vx)
+
+        print("// prototypes")
+        for prototype in [vuintm_vror_vv_prototype, vuintm_vror_vx_prototype, vuintm_vrol_vv_prototype, vuintm_vrol_vx_prototype]:
+            print(generate_intrinsic_prototype(prototype))
+        print("\n// intrinsics")
+        for prototype, emulation in [(vuintm_vror_vv_prototype, vuintm_vror_vv_emulation), (vuintm_vror_vx_prototype, vuintm_vror_vx_emulation), (vuintm_vrol_vv_prototype, vuintm_vrol_vv_emulation), (vuintm_vrol_vx_prototype, vuintm_vrol_vx_emulation) ]:
+            print(generate_intrinsic_from_operation(prototype, emulation))
