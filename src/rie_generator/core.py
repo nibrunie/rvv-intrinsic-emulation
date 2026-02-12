09e30806e8545d463a61430f7b60e7bb30e7b773
Nicolas Brunie <nibrunie@gmail.com>
2026-02-11 19:54:33 -0800

    Adding core support for tail and mask policies

--- src/rie_generator/core.py
+++ src/rie_generator/core.py
@@ -50,6 +50,25 @@ class LMULType(Enum):
         else:
             raise ValueError("Invalid LMUL type")
 
+    @staticmethod
+    def to_value(lmul_type: 'LMULType') -> float:
+        if lmul_type == LMULType.MF8:
+            return 0.125
+        elif lmul_type == LMULType.MF4:
+            return 0.25
+        elif lmul_type == LMULType.MF2:
+            return 0.5
+        elif lmul_type == LMULType.M1:
+            return 1
+        elif lmul_type == LMULType.M2:
+            return 2
+        elif lmul_type == LMULType.M4:
+            return 4
+        elif lmul_type == LMULType.M8:
+            return 8
+        else:
+            raise ValueError("Invalid LMUL type")
+
 class OperationType(Enum):
     ROR = auto()
     ROL = auto()
@@ -126,31 +145,13 @@ class Node:
         self.node_type = NodeType.UNDEFINED
         self.node_format = None
 
-class Immediate(Node):
-    def __init__(self, node_format: 'NodeFormatDescriptor', value: int):
-        self.node_format = node_format
-        self.value = value
-        self.node_type = NodeType.IMMEDIATE
-
-class Input(Node):
-    def __init__(self, node_format: 'NodeFormatDescriptor', index: int, name: str = None  ):
-        self.node_format = node_format
-        self.index = index
-        self.name = name
-        self.node_type = NodeType.INPUT
-
-class Operation(Node):
-    def __init__(self, node_format: 'NodeFormatDescriptor', op_desc: OperationDesciptor, *args):
-        self.node_format = node_format
-        self.op_desc = op_desc
-        self.args = args
-        self.node_type = NodeType.OPERATION
 
 class NodeFormatType(Enum):
     VECTOR = auto()
     SCALAR = auto()
     IMMEDIATE = auto()
     VECTOR_LENGTH = auto()
+    MASK = auto()
 
 class NodeType(Enum):
     INPUT = auto()
@@ -163,6 +164,43 @@ class NodeFormatDescriptor:
         self.node_format_type = node_format_type
         self.elt_type = elt_type
         self.lmul_type = lmul_type
+
+
+class Immediate(Node):
+    def __init__(self, node_format: NodeFormatDescriptor, value: int):
+        self.node_format = node_format
+        self.value = value
+        self.node_type = NodeType.IMMEDIATE
+
+class Input(Node):
+    def __init__(self, node_format: NodeFormatDescriptor, index: int, name: str = None  ):
+        self.node_format = node_format
+        self.index = index
+        self.name = name
+        self.node_type = NodeType.INPUT
+
+class TailPolicy(Enum):
+    AGNOSTIC = auto()
+    UNDISTURBED = auto()
+    UNDEFINED = auto()
+
+class MaskPolicy(Enum):
+    AGNOSTIC = auto()
+    UNDISTURBED = auto()
+    UNDEFINED = auto()
+    
+    
+
+class Operation(Node):
+    def __init__(self, node_format: NodeFormatDescriptor, op_desc: OperationDesciptor, *args, vm: Input=None, dst: Input=None, tail_policy: TailPolicy=TailPolicy.UNDEFINED, mask_policy: MaskPolicy=MaskPolicy.UNDEFINED):
+        self.node_format = node_format
+        self.op_desc = op_desc
+        self.args = args
+        self.node_type = NodeType.OPERATION
+        self.vm = vm
+        self.dst = dst
+        self.tail_policy = tail_policy
+        self.mask_policy = mask_policy
 
 def element_size(elt_type: EltType) -> int:
     if elt_type == EltType.U8 or elt_type == EltType.S8:
@@ -178,6 +216,9 @@ def element_size(elt_type: EltType) -> int:
 
 def get_scalar_format(node_format: NodeFormatDescriptor) -> NodeFormatDescriptor:
     return NodeFormatDescriptor(NodeFormatType.SCALAR, node_format.elt_type, None)
+
+def get_mask_format(node_format: NodeFormatDescriptor) -> NodeFormatDescriptor:
+    return NodeFormatDescriptor(NodeFormatType.MASK, node_format.elt_type, node_format.lmul_type)
 
 
 def int_type_to_scalar_type(int_type: EltType) -> str:
@@ -216,6 +257,12 @@ def int_type_to_vector_type(int_type: EltType, lmul_type: LMULType) -> str:
     else:
         raise ValueError("Invalid integer type")
 
+def vector_type_to_mask_type(node_format: NodeFormatDescriptor) -> str:
+    elt_size = element_size(node_format.elt_type)
+    lmul_value = LMULType.to_value(node_format.lmul_type)
+    n = int(elt_size / lmul_value)
+    return f"vbool{n}_t"
+
 def generate_node_format_type_string(node_format: NodeFormatDescriptor) -> str:
     if node_format.node_format_type == NodeFormatType.VECTOR:
         return int_type_to_vector_type(node_format.elt_type, node_format.lmul_type)
@@ -225,6 +272,8 @@ def generate_node_format_type_string(node_format: NodeFormatDescriptor) -> str:
         return int_type_to_scalar_type(node_format.elt_type)
     elif node_format.node_format_type == NodeFormatType.VECTOR_LENGTH:
         return "size_t"
+    elif node_format.node_format_type == NodeFormatType.MASK:
+        return vector_type_to_mask_type(node_format)
     else:
         raise ValueError("Invalid operand type")
 
@@ -253,7 +302,19 @@ def generate_intrinsic_name(prototype: Operation) -> str:
             operand_type_descriptor += "x"
         elif arg.node_format.node_format_type == NodeFormatType.IMMEDIATE:
             operand_type_descriptor += "i"
-    intrinsic_name = f"__riscv_v{OperationType.to_string(prototype.op_desc.op_type)}_{operand_type_descriptor}_{intrinsic_type_tag}"
+    suffix = ""
+    # in rvv-intrinsics-doc, tail policy always come before mask policy
+    # TODO: handle tail and mask AGNOSTIC policies
+    if prototype.tail_policy == TailPolicy.UNDISTURBED:
+        suffix += "tu"
+    elif prototype.tail_policy == TailPolicy.AGNOSTIC:
+        suffix += "t"
+    if prototype.mask_policy == MaskPolicy.AGNOSTIC:
+        suffix += "m"
+    elif prototype.mask_policy == MaskPolicy.UNDISTURBED:
+        suffix += "mu"
+    suffix = f"_{suffix}" if suffix != "" else ""
+    intrinsic_name = f"__riscv_v{OperationType.to_string(prototype.op_desc.op_type)}_{operand_type_descriptor}_{intrinsic_type_tag}{suffix}"
     return intrinsic_name
 
 def generate_intrinsic_prototype(prototype: Operation) -> str:
@@ -262,6 +323,12 @@ def generate_intrinsic_prototype(prototype: Operation) -> str:
     # generate prototype
     dst_type = generate_node_format_type_string(prototype.node_format)
     src_types = [generate_node_format_type_string(arg.node_format) for arg in prototype.args]
+    if prototype.tail_policy == TailPolicy.UNDISTURBED or prototype.mask_policy == MaskPolicy.UNDISTURBED:
+        assert prototype.dst is not None
+        src_types = [generate_node_format_type_string(prototype.dst.node_format)] + src_types
+    # in rvv-intrinsics-doc, vm come before tail (arguments order)
+    if prototype.mask_policy != MaskPolicy.UNDEFINED:
+        src_types = [generate_node_format_type_string(prototype.vm.node_format)] + src_types
     prototype = f"{dst_type} {intrinsic_name}({', '.join(src_types)})"
     return f"{prototype};"
 
@@ -289,7 +356,15 @@ def generate_operation(code: CodeObject, op: Node, memoization_map: dict[str]) -
         if op in memoization_map:
             return memoization_map[op]
         elif op.node_format.node_format_type == NodeFormatType.VECTOR or any(arg.node_format.node_format_type == NodeFormatType.VECTOR for arg in op.args):
-            call_op = f"{generate_intrinsic_name(op)}({', '.join([generate_operation(code, arg, memoization_map) for arg in op.args])})"
+            # generate intrinsic call
+            intrinsic_arg_list = [generate_operation(code, arg, memoization_map) for arg in op.args]
+            if op.tail_policy == TailPolicy.UNDISTURBED or op.mask_policy == MaskPolicy.UNDISTURBED:
+                assert op.dst is not None
+                intrinsic_arg_list.insert(0, generate_operation(code, op.dst, memoization_map))
+            if op.vm is not None:
+                intrinsic_arg_list.insert(0, generate_operation(code, op.vm, memoization_map))
+            
+            call_op = f"{generate_intrinsic_name(op)}({', '.join(intrinsic_arg_list)})"
             # generate temp variable
             temp_var = code.allocate_new_free_var()
             memoization_map[op] = temp_var
@@ -365,6 +440,13 @@ def generate_intrinsic_from_operation(prototype: Operation, emulation: Operation
             return f"op{src.index}"
     src_list = [f"{src_type} {get_src_name(src)}" for src, src_type in zip(prototype.args, src_types)]
     memoisation_map = {src: get_src_name(src) for src in prototype.args}
+    if prototype.tail_policy == TailPolicy.UNDISTURBED or prototype.mask_policy == MaskPolicy.UNDISTURBED:
+        assert prototype.dst is not None
+        src_list.insert(0, f"{dst_type} {get_src_name(prototype.dst)}")
+        memoisation_map[prototype.dst] = get_src_name(prototype.dst)
+    if prototype.vm is not None:
+        src_list.insert(0, f"{generate_node_format_type_string(prototype.vm.node_format)} {get_src_name(prototype.vm)}")
+        memoisation_map[prototype.vm] = get_src_name(prototype.vm)
     header = f"{dst_type} {intrinsic_name}({', '.join(src_list)}) {{\n"
     code = CodeObject("")
     result = generate_operation(code, emulation, memoisation_map)
